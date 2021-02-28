46098ef3dcd755c5e5830e9a818c62e6a65e8238
Vincent Orback <hello@vincentorback.se>
2021-02-28 20:34:26 +0100

    add xcode installation to update function

--- fish/.config/fish/functions/update.fish
+++ fish/.config/fish/functions/update.fish
@@ -1,7 +1,8 @@
 function update --description 'Get OS X Software Updates and update Homebrew, mas, and npm packages'
   sudo softwareupdate -i -a
+  xcode-select --install
   brew update
-  brew upgrade --force --all
+  brew upgrade
   brew cleanup
   mas upgrade
   npm install npm -g
